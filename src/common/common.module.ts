5423d7d3ed86b52f81c86b121df4158885440e4d
alfarezyyd <adityaalfarezyd@gmail.com>
2024-11-12 22:38:46 +0700

    feat: add logger

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -3,9 +3,26 @@ import { ConfigModule } from '@nestjs/config';
 
 import PrismaService from './prisma.service';
 import ValidationService from './validation.service';
+import { WinstonModule } from 'nest-winston';
+import winston from 'winston';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    WinstonModule.forRoot({
+      level: 'debug',
+      format: winston.format.combine(winston.format.json()),
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.colorize(),
+            winston.format.simple(),
+          ),
+        }),
+      ],
+    }),
+  ],
   providers: [PrismaService, ValidationService],
   exports: [PrismaService, ValidationService],
 })
