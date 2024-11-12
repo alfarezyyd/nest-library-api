7d130e73691c73a38c05b3a8715ce521b7528f01
alfarezyyd <adityaalfarezyd@gmail.com>
2024-11-13 00:41:16 +0700

    fix: import

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -4,18 +4,20 @@ import { ConfigModule } from '@nestjs/config';
 import PrismaService from './prisma.service';
 import ValidationService from './validation.service';
 import { WinstonModule } from 'nest-winston';
-import winston from 'winston';
+import * as winston from 'winston';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     WinstonModule.forRoot({
       level: 'debug',
-      format: winston.format.combine(winston.format.json()),
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
+      ),
       transports: [
         new winston.transports.Console({
           format: winston.format.combine(
-            winston.format.timestamp(),
             winston.format.colorize(),
             winston.format.simple(),
           ),
