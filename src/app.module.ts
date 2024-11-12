7a6cc8842df0b98d070cb914e13d32e747ab372a
alfarezyyd <adityaalfarezyd@gmail.com>
2024-11-12 22:12:32 +0700

    feat: app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookModule } from './book/book.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [BookModule],
+  imports: [BookModule, CategoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
