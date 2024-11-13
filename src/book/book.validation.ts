66b69cb7d2719ad994f32af8498249e916603099
alfarezyyd <adityaalfarezyd@gmail.com>
2024-11-14 04:01:47 +0700

    feat: coerce to convert

--- src/book/book.validation.ts
+++ src/book/book.validation.ts
@@ -2,12 +2,12 @@ import { z, ZodType } from 'zod';
 
 export class BookValidation {
   static readonly SAVE: ZodType = z.object({
-    categoryId: z.number().min(1),
+    categoryId: z.coerce.number().min(1),
     title: z.string().min(1),
     author: z.string().min(1),
     publisher: z.string().min(1),
     publicationYear: z.string().length(4),
-    pagesAmount: z.number().min(1),
+    pagesAmount: z.coerce.number().min(1),
     description: z.string(),
   });
 }
