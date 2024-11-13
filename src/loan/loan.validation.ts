d30242d92b7ce3f8600b507a0f712ca2a1305842
alfarezyyd <adityaalfarezyd@gmail.com>
2024-11-13 13:43:23 +0700

    feat: loan validation

---
+++ src/loan/loan.validation.ts
@@ -0,0 +1,22 @@
+import { z, ZodType } from 'zod';
+
+export class LoanValidation {
+  static readonly SAVE: ZodType = z.object({
+    bookId: z.number().gte(1),
+    loanDate: z
+      .string()
+      .refine((val) => !isNaN(Date.parse(val)), {
+        message: 'Invalid Date Format',
+      })
+      .transform((val) => new Date(val)),
+  });
+  static readonly UPDATE: ZodType = z.object({
+    bookId: z.number().gte(1),
+    returnDate: z
+      .string()
+      .refine((val) => !isNaN(Date.parse(val)), {
+        message: 'Invalid Date Format',
+      })
+      .transform((val) => new Date(val)),
+  });
+}
