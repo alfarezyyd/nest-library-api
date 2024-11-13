13b30ccd3791039fa3345e30894a43ccbfd35c47
alfarezyyd <adityaalfarezyd@gmail.com>
2024-11-14 05:18:50 +0700

    feat: change validation rule

--- src/book/book.validation.ts
+++ src/book/book.validation.ts
@@ -2,7 +2,7 @@ import { z, ZodType } from 'zod';
 
 export class BookValidation {
   static readonly SAVE: ZodType = z.object({
-    categoryId: z.coerce.number().min(1),
+    categoryId: z.coerce.number().gte(1),
     title: z.string().min(1),
     author: z.string().min(1),
     publisher: z.string().min(1),
