1608f079f2abefe5c5dd900470186c31bb41efc7
alfarezyyd <adityaalfarezyd@gmail.com>
2024-11-13 11:26:35 +0700

    feat: import loan module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { BookModule } from './book/book.module';
 import { CategoryModule } from './category/category.module';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { UserModule } from './user/user.module';
+import { LoanModule } from './loan/loan.module';
 
 @Module({
-  imports: [BookModule, CategoryModule, AuthenticationModule, UserModule],
+  imports: [BookModule, CategoryModule, AuthenticationModule, UserModule, LoanModule],
   controllers: [AppController],
   providers: [AppService],
 })
