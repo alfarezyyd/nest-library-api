7e943cbf009fd5aa044a262dd9fe38f204bc4303
alfarezyyd <adityaalfarezyd@gmail.com>
2024-11-13 19:49:46 +0700

    feat: add filter

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import ValidationExceptionFilter from './exception/ValidationExceptionFilter';
 import PrismaExceptionFilter from './exception/PrismaExceptionFilter';
+import { HttpExceptionFilter } from './exception/HttpExceptionFilter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Exception Filter
   app.useGlobalFilters(new PrismaExceptionFilter());
   app.useGlobalFilters(new ValidationExceptionFilter());
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   BigInt.prototype['toJSON'] = function () {
     return this.toString();
