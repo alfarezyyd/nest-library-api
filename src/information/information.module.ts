e49172666201a9e631b2b9f450ef398deaad149c
alfarezyyd <adityaalfarezyd@gmail.com>
2024-11-14 14:59:46 +0700

    feat: import common module

--- src/information/information.module.ts
+++ src/information/information.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { InformationService } from './information.service';
 import { InformationController } from './information.controller';
+import { CommonModule } from '../common/common.module';
 
 @Module({
+  imports: [CommonModule],
   controllers: [InformationController],
   providers: [InformationService],
 })
