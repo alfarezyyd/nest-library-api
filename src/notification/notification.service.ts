21db06d189ee36f44423bdba731ac0a8e7324ff0
alfarezyyd <adityaalfarezyd@gmail.com>
2024-11-15 01:38:28 +0700

    fix: wrong logic at trigger

--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -1,36 +1,57 @@
 import { Injectable } from '@nestjs/common';
 import { UpdateNotificationDto } from './dto/update-notification.dto';
-import { Loan, LoanStatus, User } from '@prisma/client';
+import { LoanStatus, User } from '@prisma/client';
 import PrismaService from '../common/prisma.service';
 
 @Injectable()
 export class NotificationService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  handleTriggerNotification(currentUser: User) {
-    this.prismaService.loan.findMany({
+  async handleTriggerNotification(currentUser: User) {
+    const loansPrisma = await this.prismaService.loan.findMany({
       where: {
         userId: currentUser.id,
         loanStatus: LoanStatus.BORROWED,
         OR: [
           {
-            returnDate: {
+            loanDate: {
               lte: new Date(new Date().getTime() - 7 * 24 * 60 * 60 * 1000),
             }, // lewat 7 hari
           },
           {
-            returnDate: {
+            loanDate: {
               gte: new Date(new Date().getTime() + 24 * 60 * 60 * 1000),
               lte: new Date(new Date().getTime() + 3 * 24 * 60 * 60 * 1000),
             }, // 1-3 hari sebelum 7 hari
           },
         ],
       },
+      include: {
+        book: true,
+      },
     });
+    console.log(loansPrisma, currentUser);
+    const notificationPayload = [];
+    for (const loanPrisma of loansPrisma) {
+      notificationPayload.push({
+        userId: currentUser.id,
+        headerMessage: `Mohon Kembalikan Buku`,
+        message: `Tolong kembalikan buku ${loanPrisma.book.title}, jika Anda melewati jatuh tempo maka denda akan dikenakan`,
+      });
+    }
+    await this.prismaService.userNotification.createMany({
+      data: notificationPayload,
+    });
+    return true;
   }
 
-  findAll() {
-    return `This action returns all notification`;
+  async findAllByUserId(currentUser: User) {
+    return this.prismaService.userNotification.findMany({
+      where: {
+        userId: currentUser.id,
+      },
+      include: {},
+    });
   }
 
   findOne(id: number) {
